9f26fa26e8b0af6827b78add4ee7c9a2e25d4d03
Dragan Mirkovic <draganmirkovic2402@gmail.com>
2024-05-15 21:37:22 +0200

    Feature 44: Added type interfaces for entitites

--- fullstack/generator/util/filters.py
+++ fullstack/generator/util/filters.py
@@ -1,9 +1,15 @@
-from fullstack.language.builtins import simple_type_java_mapper
+from fullstack.language.builtins import simple_type_java_mapper, simple_types_typescript
 
 
 def format_type(obj_type):
     if obj_type.__class__.__name__ == 'SimpleType':
         return simple_type_java_mapper.get(obj_type.name)
+    else:
+        return obj_type.name
+    
+def format_type_typescript(obj_type):
+    if obj_type.__class__.__name__ == 'SimpleType':
+        return simple_types_typescript.get(obj_type.name)
     else:
         return obj_type.name
 
